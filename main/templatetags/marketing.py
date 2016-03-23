5047442f53276534d3f056f2690a870894e50d8c
Munk <munk801@gmail.com>
2016-03-22 22:36:34 -0700

    Created base template tag for marketing and abstracted marketing item to its own file.

---
+++ main/templatetags/marketing.py
@@ -0,0 +1,13 @@
+from django import template
+register = template.Library()
+
+@register.inclusion_tag('circle_item.html')
+def circle_header_item(img_name='marketing_0.png', heading='Test', caption='Test',
+					   button_link="register", button_title="View details"):
+	return {
+		'img': img_name,
+		'heading': heading,
+		'caption': caption,
+		'button_link': button_link,
+		'button_title': button_title
+	}
